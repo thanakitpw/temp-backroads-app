769dac52b91df27c810fd2c8ca88ce38fe2bfc1f
thanakitpw <thanakitpw@gmail.com>
2024-08-25 18:02:49 +0700

    first commit

---
+++ src/components/SocialLink.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SocialLink = ({ href, icon, itemClass }) => {
+   return (
+      <li>
+         <a href={href} rel="noreferrer" target="_blank" className={itemClass}>
+            <i className={icon}></i>
+         </a>
+      </li>
+   );
+};
+
+export default SocialLink;
---
+++ src/data.js
@@ -0,0 +1,118 @@
+import tour1 from "./images/tour-1.jpeg";
+
+export const pageLinks = [
+   { id: 1, href: "#home", text: "home" },
+   { id: 2, href: "#about", text: "about" },
+   { id: 3, href: "#services", text: "services" },
+   { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+   { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+   { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+   { id: 1, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+   {
+      id: 1,
+      icon: "fas fa-wallet fa-fw",
+      title: "saving money",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+   },
+   {
+      id: 2,
+      icon: "fas fa-tree fa-fw",
+      title: "endless hiking",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+   },
+   {
+      id: 3,
+      icon: "fas fa-socks fa-fw",
+      title: "amazing comfort",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+   },
+];
+
+export const tours = [
+   {
+      id: 1,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 2,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 1,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 3,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 1,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 4,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 5,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+   {
+      id: 6,
+      image: tour1,
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque vitae tempore voluptatum maxime reprehenderit eumquod exercitationem fugit, qui corporis.",
+      location: "china",
+      duration: 6,
+      price: 2100,
+   },
+];
